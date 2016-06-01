2d1fb2edfba8ab458674f12cb263a78086f0ef55
Kristóf Poduszló <kripod@protonmail.com>
2016-06-01 08:10:27 +0200

    Added separate console timers for each operation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import Deck from './deck';
 import Game from './game';
 import Player from './player';
 
@@ -9,35 +8,44 @@ export {
 export default async function () {
   const PLAYER_COUNT = 4;
 
+  console.time('3.1.1');
   const players = Array.from({ length: PLAYER_COUNT }).map(() => new Player());
   const game = new Game(players);
 
+  console.timeEnd('3.1.1');
   console.log('3.1.1 done');
 
   // 3.1.2 - Cascaded shuffling
+  console.time('3.1.2');
   let deck = game.deckOriginal;
   for (const player of players) {
     deck = player.shuffleDeck(deck);
   }
 
+  console.timeEnd('3.1.2');
   console.log('3.1.2 done');
 
   // 3.1.3 - Locking
+  console.time('3.1.3');
   for (const player of players) {
     deck = player.encryptDeck(deck);
   }
 
   game.deckEncrypted = deck;
+  console.timeEnd('3.1.3');
   console.log('3.1.3 done');
 
   // 3.2 - Drawing
+  console.time('3.2');
   const index = 42;
   const secrets = [];
   for (const player of players) {
     secrets.push(player.secrets[index]);
   }
 
+  console.log(game.deckOriginal.points);
   console.log(deck.decryptSingle(index, secrets));
   console.log(game.drawCard(index, secrets));
+  console.timeEnd('3.2');
   console.log('3.2 done');
 }
